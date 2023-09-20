86b93693d05667c2537b64cc784c484e0c60534e
9777717 <zbb1064212882@126.com>
2023-09-20 15:05:48 +0800

    fix: 4

--- ops/lib/utils.js
+++ ops/lib/utils.js
@@ -91,7 +91,7 @@ class WranglerCmd {
       response.on('end', () => {
         const body = JSON.parse(data);
         let databaseId = '';
-        console.log(data,'data-----------------------');
+        console.log(accountId,apiKey,data,'data-----------------------');
         body.result.forEach((result) => {
           if (result.name === dbName) {
             databaseId = result.uuid;
